2d9a0ad18eda69e6748338bc2476855e8324db9c
Antonio Granaldi <tonio.granaldi@gmail.com>
2024-10-03 21:46:53 +0200

    Changed default theme color to match user preference

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -2,19 +2,23 @@ import { persistentAtom } from '@nanostores/persistent';
 import { atom } from 'nanostores';
 
 export const darkTheme = 'dark';
-
 export const lightTheme = 'light';
 
 type Theme = typeof darkTheme | typeof lightTheme;
 
-export const defaultTheme: Theme = lightTheme;
+export const defaultTheme: Theme =
+	typeof window !== 'undefined' &&
+	window.matchMedia &&
+	window.matchMedia('(prefers-color-scheme: dark)').matches
+		? darkTheme
+		: lightTheme;
 
 export const theme = persistentAtom<Theme>('theme', defaultTheme);
 
 export const isThemeReady = atom(false);
 
 export const setTheme = () => {
-	if (document !== undefined && localStorage !== undefined) {
+	if (typeof document !== 'undefined' && typeof localStorage !== 'undefined') {
 		document.documentElement.classList.add(theme.get());
 		document.documentElement.classList.remove(
 			theme.get() === darkTheme ? lightTheme : darkTheme
@@ -28,3 +32,13 @@ export const toggleTheme = () => {
 	theme.set(theme.get() === darkTheme ? lightTheme : darkTheme);
 	setTheme();
 };
+
+if (typeof window !== 'undefined' && window.matchMedia) {
+	const darkThemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+	darkThemeMediaQuery.addEventListener('change', (e) => {
+		const newTheme = e.matches ? darkTheme : lightTheme;
+		theme.set(newTheme);
+		setTheme();
+	});
+}
